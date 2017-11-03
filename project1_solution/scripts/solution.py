b171d4ac35c1f3982c21d23d461e781c09824468
Robotics <nthdimensionalreality@gmail.com>
2017-11-03 13:25:41 -0600

    1. Implemented publisher using class structure  2. Separate package from messages were created 3. Created a launch file 4. few modification were made in both nodes for better readability

--- project1_solution/scripts/solution.py
+++ project1_solution/scripts/solution.py
@@ -2,7 +2,8 @@
 import rospy
 
 from std_msgs.msg import Int16
-from project1_solution.msg import TwoInts
+# from project1_solution.msg import TwoInts
+from project1_solution_msgs.msg import TwoInts
 
 pub = rospy.Publisher('sum', Int16, queue_size=10)
 
@@ -12,11 +13,15 @@ def callback(data):
     pub.publish(summation)
 
 
-def listener():
-    rospy.init_node('sum_two_ints', anonymous=True)
+def summer():
+    # Create a subscriber with appropriate topic, custom message and name of callback function.
     rospy.Subscriber("two_ints", TwoInts, callback)
+    # Wait for messages on topic, go to callback function when new messages arrive.
     rospy.spin()
 
 
 if __name__ == '__main__':
-    listener()
+    # Initialize the node and name it.
+    rospy.init_node('add_two_ints')
+    # Go to the main loop.
+    summer()
--- two_int_talker/scripts/two_int_talker.py
+++ two_int_talker/scripts/two_int_talker.py
@@ -2,28 +2,32 @@
 import random
 
 import rospy
-from std_msgs.msg import Int16
-from project1_solution.msg import TwoInts
+# from std_msgs.msg import Int16
+# from project1_solution.msg import TwoInts
+from project1_solution_msgs.msg import TwoInts
 
 
-def talker():
-    pub = rospy.Publisher('two_ints', TwoInts, queue_size=10)
-    rospy.init_node('two_int_talker', anonymous=True)
-    rate = rospy.Rate(0.5)
-    random.seed()
-
-    while not rospy.is_shutdown():
-
+class Talker:
+    # Must have __init__(self) function for a class, similar to a C++ class constructor
+    def __init__(self):
+        rate = rospy.Rate(0.5)
+        pub = rospy.Publisher('two_ints', TwoInts, queue_size=10)
+        # Set the message to publish as our custom message.
         msg = TwoInts()
-  
-        msg.a = random.randint(1,20)
-        msg.b = random.randint(1,20)
-        pub.publish(msg)
-        rate.sleep()
-    
+        # Main while loop.
+        while not rospy.is_shutdown():
+            # Initialize message variables.
+            msg.a = random.randint(1, 20)
+            msg.b = random.randint(1, 20)
+            pub.publish(msg)
+            rate.sleep()
+
 
 if __name__ == '__main__':
+    # Initialize the node and name it
+    rospy.init_node('two_int_talker', anonymous=False)
+    # Go to class functions that do all the heavy lifting. Do error checking.
     try:
-        talker()
+        Talker()
     except rospy.ROSInterruptException:
         pass
