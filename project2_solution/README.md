d447061b09b062f0baf4ba2b8ce13f2139ee31f1
BhanuKiranChaluvadi <nthdimensionalreality@gmail.com>
2017-11-04 13:42:56 -0600

    1. Wrote a function: message_from_transformations to handle repeated actions for each frame. 2. Introduced matrix concatenation to perform desired sequence of operations and inputted in the same order (check T1, T2 rotation is performed first and for T3 translation is performed first) .

--- project2_solution/README.md
+++ project2_solution/README.md
@@ -1,11 +1,24 @@
 # Transformations
 publishing static tranformations and visualizing in Rviz.
 
-project description:
+# project description: Static transformations:
 https://courses.edx.org/courses/course-v1:ColumbiaX+CSMM.103x+1T2017/courseware/c3368f33e7ca49cfb0a2c3275f6fbda0/c64f51da0c2d41b19dc5479f9d74222d/?child=first
+1. Node publish static transformation to tf topic.
+2. rViz is used to visualize the co-ordinate frames by subscribing to tf and by using markerss.
 
-Tutorials:
+# marker_publisher.cpp
+If we observer the file we find all the position and orientaion of all the markers are set to zero.
+But in rViz their poisiton and orientaion are nicely assingned. The trick lies in the "object.header.frame_id" that 
+was set accordingly. rViz will handle tansformations accordingly.
+
+# Tutorials:
 http://wiki.ros.org/tf2/Tutorials
 http://wiki.ros.org/tf2/Tutorials/Writing%20a%20tf2%20static%20broadcaster%20%28Python%29
 
+# Learnings:
 
+1. Rotation and then translation should be performed - operations are performed in the way the matrices are entered in the concatenation matrix.
+2. In normal transformation t1.transformation - tranformation is always performed first and then rotaion irrespective of how you enter (in example.py).
+3. Yaw-pitch - row angle its zyx. First the operations are performed on the z-axis and then Y , then X.
+4. The rotation is performed using axis angle - dot, cross product between the current and desired axis results in axis angle.
+4. In transformations.quaternion_about_axis() . The angle is entered in radians.
--- project2_solution/scripts/solution.py
+++ project2_solution/scripts/solution.py
@@ -7,24 +7,23 @@ import tf
 import tf2_ros
 import geometry_msgs.msg
 
-import numpy
 
+def message_from_transform(T):
+    msg = geometry_msgs.msg.Transform()
+    q = tf.transformations.quaternion_from_matrix(T)
+    translation = tf.transformations.translation_from_matrix(T)
+    msg.translation.x = translation[0]
+    msg.translation.y = translation[1]
+    msg.translation.z = translation[2]
+    msg.rotation.x = q[0]
+    msg.rotation.y = q[1]
+    msg.rotation.z = q[2]
+    msg.rotation.w = q[3]
+    return msg
 
-# The transform from the 'base' coordinate frame to the 'object' coordinate frame consists of a rotation expressed
-# as (roll, pitch, yaw) of (0.79, 0.0, 0.79) followed by a translation of 1.0m along the resulting y-axis and 1.0m
-# along the resulting z-axis.
-
-
-# The transform from the 'base' coordinate frame to the 'robot' coordinate frame consists of a rotation around the
-# z-axis by 1.5 radians followed by a translation along the resulting y-axis of -1.0m.
-#
-# The transform from the 'robot' coordinate frame to the 'camera' coordinate frame must be defined as follows:
-#     1. The translation component of this transform is (0.0, 0.1, 0.1)
-#     2. The rotation component of this transform must be set such that the camera is pointing directly at the object.
-#     In other words, the x-axis of the 'camera' coordinate frame must be pointing directly at
-#     the origin of the 'object' coordinate frame.
 
 def publish_transforms():
+
     # 1. base_frame to object_frame
     object_transform = geometry_msgs.msg.TransformStamped()
 
@@ -32,15 +31,13 @@ def publish_transforms():
     object_transform.header.frame_id = "base_frame"
     object_transform.child_frame_id = "object_frame"
 
-    object_transform.transform.translation.x = 0.0
-    object_transform.transform.translation.y = 1.0
-    object_transform.transform.translation.z = 1.0
+    T1 = tf.transformations.concatenate_matrices(
+        tf.transformations.quaternion_matrix(
+            tf.transformations.quaternion_from_euler(0.79, 0.0, 0.79)),
+        tf.transformations.translation_matrix((0.0, 1.0, 1.0))
+    )
 
-    q1 = tf.transformations.quaternion_from_euler(0.79, 0.0, 0.79)
-    object_transform.transform.rotation.x = q1[0]
-    object_transform.transform.rotation.y = q1[1]
-    object_transform.transform.rotation.z = q1[2]
-    object_transform.transform.rotation.w = q1[3]
+    object_transform.transform = message_from_transform(T1)
 
     broadcaster.sendTransform(object_transform)
 
@@ -50,15 +47,13 @@ def publish_transforms():
     robot_transform.header.frame_id = "base_frame"
     robot_transform.child_frame_id = "robot_frame"
 
-    robot_transform.transform.translation.x = 0.0
-    robot_transform.transform.translation.y = -1.0
-    robot_transform.transform.translation.z = 0.0
+    T2 = tf.transformations.concatenate_matrices(
+        tf.transformations.quaternion_matrix(
+            tf.transformations.quaternion_about_axis(1.57, (0, 0, 1))),
+        tf.transformations.translation_matrix((0.0, -1.0, 0.0))
+    )
 
-    q2 = tf.transformations.quaternion_about_axis(1.5, (0.0, 0.0, 1))
-    robot_transform.transform.rotation.x = q2[0]
-    robot_transform.transform.rotation.y = q2[1]
-    robot_transform.transform.rotation.z = q2[2]
-    robot_transform.transform.rotation.w = q2[3]
+    robot_transform.transform = message_from_transform(T2)
 
     broadcaster.sendTransform(robot_transform)
 
@@ -68,15 +63,13 @@ def publish_transforms():
     camera_transform.header.frame_id = "robot_frame"
     camera_transform.child_frame_id = "camera_frame"
 
-    camera_transform.transform.translation.x = 0.0
-    camera_transform.transform.translation.y = 0.1
-    camera_transform.transform.translation.z = 0.1
+    T3 = tf.transformations.concatenate_matrices(
+        tf.transformations.translation_matrix((0.0, 0.1, 0.1)),
+        tf.transformations.quaternion_matrix(
+            tf.transformations.quaternion_about_axis(1.57, (0.0, -1.314, 0.9)))
+    )
 
-    q3 = tf.transformations.quaternion_about_axis(1.5, (0.0, 0.0, 1))
-    camera_transform.transform.rotation.x = q3[0]
-    camera_transform.transform.rotation.y = q3[1]
-    camera_transform.transform.rotation.z = q3[2]
-    camera_transform.transform.rotation.w = q3[3]
+    camera_transform.transform = message_from_transform(T3)
 
     broadcaster.sendTransform(camera_transform)
 
