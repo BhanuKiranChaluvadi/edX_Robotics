f7b8ecf7d152f646eebeb3a0328cc14c470e4144
Robotics <nthdimensionalreality@gmail.com>
2017-11-09 13:49:44 -0700

    Code for finding pseudo jacobian and commnading velocities is added.

--- cartesian_control/scripts/cartesian_control.py
+++ cartesian_control/scripts/cartesian_control.py
@@ -12,14 +12,15 @@ from sensor_msgs.msg import JointState
 from std_msgs.msg import Float32
 from urdf_parser_py.urdf import URDF
 
+
 def S_matrix(w):
-    S = numpy.zeros((3,3))
-    S[0,1] = -w[2]
-    S[0,2] =  w[1]
-    S[1,0] =  w[2]
-    S[1,2] = -w[0]
-    S[2,0] = -w[1]
-    S[2,1] =  w[0]
+    S = numpy.zeros((3, 3))
+    S[0, 1] = -w[2]
+    S[0, 2] = w[1]
+    S[1, 0] = w[2]
+    S[1, 2] = -w[0]
+    S[2, 0] = -w[1]
+    S[2, 1] = w[0]
     return S
 
 
@@ -71,19 +72,44 @@ def cartesian_control(joint_transforms, b_T_ee_current, b_T_ee_desired,
     # #####          Numerically compute - Robot Jacobian           ######################
     # ####################################################################################
 
+    J = []
+
+    for index, base_T_joint in enumerate(joint_transforms):
+        joint_T_base = numpy.linalg.inv(base_T_joint)
+        joint_T_ee = tf.transformations.concatenate_matrices(
+            joint_T_base,
+            b_T_ee_current
+        )
+
+        joint_translation_ee = joint_T_ee[:3, 3]
+        ee_Rot_joint = numpy.transpose(joint_T_ee[:3, :3])
+
+        # figuring out last column
+        top_right_matrix = numpy.dot(-1*ee_Rot_joint, S_matrix(joint_translation_ee))
+
+        J_col = numpy.concatenate([top_right_matrix[:, -1], ee_Rot_joint[:, -1]], axis=0)
+
+        J.append(J_col)
+
+    J = numpy.transpose(numpy.asarray(J))
+
+    pseudo_J = numpy.linalg.pinv(J, 0.01)
+
+    dq = numpy.dot(pseudo_J, V_ee)
+
     # ----------------------------------------------------------------------
     return dq
 
 
 def convert_from_message(t):
     trans = tf.transformations.translation_matrix((t.translation.x,
-                                                  t.translation.y,
-                                                  t.translation.z))
+                                                   t.translation.y,
+                                                   t.translation.z))
     rot = tf.transformations.quaternion_matrix((t.rotation.x,
                                                 t.rotation.y,
                                                 t.rotation.z,
                                                 t.rotation.w))
-    T = numpy.dot(trans,rot)
+    T = numpy.dot(trans, rot)
     return T
 
 
@@ -107,17 +133,16 @@ def rotation_from_matrix(matrix):
     # rotation angle depending on axis
     cosa = (numpy.trace(R33) - 1.0) / 2.0
     if abs(axis[2]) > 1e-8:
-        sina = (R[1, 0] + (cosa-1.0)*axis[0]*axis[1]) / axis[2]
+        sina = (R[1, 0] + (cosa - 1.0) * axis[0] * axis[1]) / axis[2]
     elif abs(axis[1]) > 1e-8:
-        sina = (R[0, 2] + (cosa-1.0)*axis[0]*axis[2]) / axis[1]
+        sina = (R[0, 2] + (cosa - 1.0) * axis[0] * axis[2]) / axis[1]
     else:
-        sina = (R[2, 1] + (cosa-1.0)*axis[1]*axis[2]) / axis[0]
+        sina = (R[2, 1] + (cosa - 1.0) * axis[1] * axis[2]) / axis[0]
     angle = math.atan2(sina, cosa)
     return angle, axis
 
 
 class CartesianControl(object):
-
     # Initialization
     def __init__(self):
         # Loads the robot model, which contains the robot's kinematics information
@@ -159,26 +184,26 @@ class CartesianControl(object):
         self.mutex.acquire()
         self.q0_desired = command.data
         self.last_red_command_time = time.time()
-        self.mutex.release()        
-        
+        self.mutex.release()
+
     def timer_callback(self, event):
         msg = JointState()
         self.mutex.acquire()
-        if time.time() - self.last_command_time > 0.5:
-            dq = cartesian_control(self.joint_transforms, 
+        if time.time() - self.last_command_time < 0.5:
+            dq = cartesian_control(self.joint_transforms,
                                    self.x_current, self.x_target,
                                    False, self.q_current, self.q0_desired)
             msg.velocity = dq
         elif time.time() - self.last_red_command_time < 0.5:
-            dq = cartesian_control(self.joint_transforms, 
+            dq = cartesian_control(self.joint_transforms,
                                    self.x_current, self.x_current,
                                    True, self.q_current, self.q0_desired)
             msg.velocity = dq
-        else:            
+        else:
             msg.velocity = numpy.zeros(7)
         self.mutex.release()
         self.pub_vel.publish(msg)
-        
+
     def joint_callback(self, joint_values):
         root = self.robot.get_root()
         T = tf.transformations.identity_matrix()
@@ -190,9 +215,9 @@ class CartesianControl(object):
 
     def align_with_z(self, axis):
         T = tf.transformations.identity_matrix()
-        z = numpy.array([0,0,1])
-        x = numpy.array([1,0,0])
-        dot = numpy.dot(z,axis)
+        z = numpy.array([0, 0, 1])
+        x = numpy.array([1, 0, 0])
+        dot = numpy.dot(z, axis)
         if dot == 1: return T
         if dot == -1: return tf.transformation.rotation_matrix(math.pi, x)
         rot_axis = numpy.cross(z, axis)
@@ -200,20 +225,20 @@ class CartesianControl(object):
         return tf.transformations.rotation_matrix(angle, rot_axis)
 
     def process_link_recursive(self, link, T, joint_values):
-        if link not in self.robot.child_map: 
+        if link not in self.robot.child_map:
             self.x_current = T
             return
-        for i in range(0,len(self.robot.child_map[link])):
+        for i in range(0, len(self.robot.child_map[link])):
             (joint_name, next_link) = self.robot.child_map[link][i]
             if joint_name not in self.robot.joint_map:
                 rospy.logerr("Joint not found in map")
                 continue
-            current_joint = self.robot.joint_map[joint_name]        
+            current_joint = self.robot.joint_map[joint_name]
 
-            trans_matrix = tf.transformations.translation_matrix((current_joint.origin.xyz[0], 
+            trans_matrix = tf.transformations.translation_matrix((current_joint.origin.xyz[0],
                                                                   current_joint.origin.xyz[1],
                                                                   current_joint.origin.xyz[2]))
-            rot_matrix = tf.transformations.euler_matrix(current_joint.origin.rpy[0], 
+            rot_matrix = tf.transformations.euler_matrix(current_joint.origin.rpy[0],
                                                          current_joint.origin.rpy[1],
                                                          current_joint.origin.rpy[2], 'rxyz')
             origin_T = numpy.dot(trans_matrix, rot_matrix)
@@ -227,14 +252,15 @@ class CartesianControl(object):
                 self.joint_transforms.append(aligned_joint_T)
                 index = joint_values.name.index(current_joint.name)
                 angle = joint_values.position[index]
-                joint_rot_T = tf.transformations.rotation_matrix(angle, 
+                joint_rot_T = tf.transformations.rotation_matrix(angle,
                                                                  numpy.asarray(current_joint.axis))
-                next_link_T = numpy.dot(current_joint_T, joint_rot_T) 
+                next_link_T = numpy.dot(current_joint_T, joint_rot_T)
             else:
                 next_link_T = current_joint_T
 
             self.process_link_recursive(next_link, next_link_T, joint_values)
-        
+
+
 if __name__ == '__main__':
     rospy.init_node('cartesian_control', anonymous=True)
     cc = CartesianControl()
